5bf8d4312dd9ed9f75116ae83bfdfaf1ed8832bf
Andy <61007039+andy-1977@users.noreply.github.com>
2026-03-31 17:30:39 +0100

    Update tpl_modules_main_product_image.php
    Changed span to div to address this:
    
    elements inside a . This is invalid HTML and can cause inconsistent rendering/JS click behavior. Use a block-level element (e.g., keep a
    wrapper) or move the click handler onto an appropriate element without nesting block elements inside a .
    
    And addressed this issue as well:
    
    The link title uses a hardcoded English string ("View larger image of …"). Zen Cart generally uses language-defined constants for customer-facing text; this should be moved to a language constant (or reuse an existing one like the click-to-enlarge text) so it can be translated.

--- includes/templates/template_default/templates/tpl_modules_main_product_image.php
+++ includes/templates/template_default/templates/tpl_modules_main_product_image.php
@@ -5,7 +5,7 @@
  * @copyright Copyright 2003-2024 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: DrByte 2024 Feb 03 Modified in v2.0.0-beta1 $
+ * @version $Id: DrByte 2025 Oct 10 Modified in v2.2.0 $
  */
 require DIR_WS_MODULES . zen_get_module_directory(FILENAME_MAIN_PRODUCT_IMAGE);
 
@@ -16,20 +16,19 @@ $main_large_image = !empty($products_image_large) ? $products_image_large : $pro
 <!-- Modal HTML -->
 <div id="imageModalPrimary" class="imgmodal">
     <div class="imgmodal-content">
-        <span onclick="closeModal('imageModalPrimary')">
+        <div onclick="closeModal('imageModalPrimary')">
             <?php echo zen_image($main_large_image, $products_name, '', '', 'class="centered-image"'); ?>
             <div class="imgmodal-close"><i class="fa-solid fa-circle-xmark"></i></div>
             <div class="center"><?php echo htmlspecialchars($products_name, ENT_QUOTES, 'UTF-8'); ?></div>
-        </span>
+        </div>
     </div>
 </div>
 
 <!-- Main product image -->
 <div id="productMainImage" class="centeredContent back">
-    <a href="<?php echo $main_large_image; ?>"
+    <a href="<?php echo htmlspecialchars($main_large_image, ENT_QUOTES, 'UTF-8'); ?>"
        onclick="openModal('imageModalPrimary'); return false;"
-       title="View larger image of <?php echo htmlspecialchars($products_name, ENT_QUOTES, 'UTF-8'); ?>"
-       rel="noopener">
+       title="<?php echo htmlspecialchars(TEXT_CLICK_TO_ENLARGE . ' ' . $products_name, ENT_QUOTES, 'UTF-8'); ?>">
        
         <?php echo zen_image($products_image_medium, $products_name, MEDIUM_IMAGE_WIDTH, MEDIUM_IMAGE_HEIGHT); ?>
         <br>

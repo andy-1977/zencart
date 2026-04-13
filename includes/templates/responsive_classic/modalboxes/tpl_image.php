3b53c764040234a8a83cb1ff25e461af67fdf357
Chris Brown <code@drbyte.dev>
2026-04-13 13:47:16 -0400

    TEXT_CLICK_TO_ENLARGE

--- includes/templates/responsive_classic/modalboxes/tpl_image.php
+++ includes/templates/responsive_classic/modalboxes/tpl_image.php
@@ -15,7 +15,7 @@ $main_large_image = !empty($products_image_large) ? $products_image_large : $pro
 <div id="productMainImage" class="centeredContent back">
     <a href="<?php echo htmlspecialchars($main_large_image, ENT_QUOTES, 'UTF-8'); ?>"
        onclick="openModal('imageModalPrimary'); return false;"
-       title="View larger image of <?php echo htmlspecialchars($products_name, ENT_QUOTES, 'UTF-8'); ?>"
+       title="<?= htmlspecialchars(TEXT_CLICK_TO_ENLARGE . ' ' . $products_name, ENT_QUOTES, 'UTF-8') ?>"
        rel="noopener">
        
         <?php echo zen_image(
